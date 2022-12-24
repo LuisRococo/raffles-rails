0cc6ccc5f21e59e5dc0069c4bb47ede6aee4625c
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-24 15:59:49 -0700

    add raffle cards

---
+++ app/javascript/react/components/home/RaffleCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function RaffleCard() {
+  return (
+    <div className="col-md-4 px-2 text-center my-2">
+      <div className="bg-light rounded border p-4">
+        <img
+          style={styles.img}
+          src="https://i.pinimg.com/originals/68/8e/45/688e4524c03c532cf821a8ca0935a6fe.jpg"
+          alt=""
+        />
+        <h3 className="mb-3">Rifa de fin de año</h3>
+        <button className="btn btn-primary btn-lg">Entrar</button>
+      </div>
+    </div>
+  );
+}
+
+const styles = {
+  img: {
+    width: "200px",
+    borderRadius: 100,
+    marginBottom: "30px",
+  },
+};
+
+export default RaffleCard;
--- app/javascript/react/pages/Home.tsx
+++ app/javascript/react/pages/Home.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Header from "../components/home/Header";
 import Message from "../components/home/Message";
+import RaffleCard from "../components/home/RaffleCard";
 
 function Home() {
   return (
     <div className="min-height">
       <Header />
       <Message />
+
+      <div className="container section">
+        <div>
+          <h2 className="section-header">Raffles</h2>
+          <hr className="section-hr" />
+        </div>
+      </div>
+
+      <div className="container mb-5">
+        <div className="row">
+          <RaffleCard />
+          <RaffleCard />
+          <RaffleCard />
+        </div>
+      </div>
     </div>
   );
 }

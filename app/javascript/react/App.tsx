5405b5fa1b779578a69741cd43b9a54d2ea03853
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-24 11:35:42 -0700

    add react router

--- app/javascript/react/App.tsx
+++ app/javascript/react/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import NavBar from "./components/common/NavBar";
 import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
-      <Home />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 

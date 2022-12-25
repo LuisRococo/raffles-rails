c298df02753c54848dd2c0aecc22d92a47b41f6c
Luis Roberto <luisrobertopenav@gmail.com>
2022-12-25 12:03:26 -0700

    refacor raffle page

---
+++ app/javascript/react/components/raffle/TicketsContainer.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import useTicketGrid from "../../hooks/TicketGridHook";
+import { IRaffleTickets } from "../../interfaces/raffleInterfaces";
+import UserTickets from "./UserTickets";
+
+interface Props {
+  raffleId: number;
+}
+
+function TicketsContainer({ raffleId }: Props) {
+  const gridFetchLimit = 200;
+  const [userTickets, setUserTickets] = useState<IRaffleTickets[]>([]);
+  const { page, gridTickets, next, previous } = useTicketGrid(
+    raffleId,
+    gridFetchLimit
+  );
+
+  function handleRemoveTicket(numberToDelete: number) {
+    let tickets = [...userTickets];
+    tickets = tickets.filter((number) => number.number !== numberToDelete);
+    setUserTickets(tickets);
+  }
+
+  function changeGrid(advance: boolean) {
+    if (advance) next();
+    else previous();
+  }
+
+  return (
+    <div>
+      <div className="my-5 text-center">
+        <h2 className="section-header mb-3">Grid</h2>
+        <p className="m-0 p-0 mb-1">You can select * numbers</p>
+        <p className="m-0 p-0 mb-1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
+          voluptatem!
+        </p>
+
+        <UserTickets removeNumber={handleRemoveTicket} numbers={userTickets} />
+
+        <hr className="my-4" />
+
+        <div>
+          <div className="d-flex justify-content-between mb-3">
+            <div className="me-3 text-start">
+              <p className="m-0 p-0">
+                <strong>Page </strong>
+                {page}
+              </p>
+              <p className="m-0 p-0 text-muted fst-italic">
+                Showing <strong>{gridFetchLimit}</strong> tickets
+              </p>
+            </div>
+            <div>
+              <button
+                onClick={() => changeGrid(false)}
+                className="btn btn-primary btn-lg"
+              >
+                {"<"}
+              </button>
+              <button
+                onClick={() => changeGrid(true)}
+                className="btn btn-primary btn-lg ms-1"
+              >
+                {">"}
+              </button>
+            </div>
+          </div>
+          <div className="tickets-container">
+            {gridTickets.length === 0 && (
+              <div>
+                <p className="m-0 p-0">
+                  <strong>There are no tickets</strong>
+                </p>
+                <p className="m-0 p-0 text-muted fst-italic">
+                  Try usign the pagination to return
+                </p>
+              </div>
+            )}
+            {gridTickets.map((ticket) => {
+              return (
+                <div key={ticket.number} className="ticket">
+                  {ticket.formatted}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TicketsContainer;
--- app/javascript/react/pages/Raffle.tsx
+++ app/javascript/react/pages/Raffle.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import TicketsContainer from "../components/raffle/TicketsContainer";
 import UserTickets from "../components/raffle/UserTickets";
 import { fetchRaffle } from "../helpers/api";
 import useTicketGrid from "../hooks/TicketGridHook";
@@ -7,31 +8,13 @@ import { RaffleResponse } from "../interfaces/apiInterfaces";
 import { IRaffleTickets } from "../interfaces/raffleInterfaces";
 
 function Raffle() {
-  const gridFetchLimit = 200;
-  const [userTickets, setUserTickets] = useState<IRaffleTickets[]>([]);
   const [raffle, setRaffle] = useState<RaffleResponse>();
   let { id: raffleId } = useParams();
-  if (!raffleId) raffleId = "";
-  const { page, gridTickets, next, previous } = useTicketGrid(
-    +raffleId,
-    gridFetchLimit
-  );
-
-  function handleRemoveTicket(numberToDelete: number) {
-    let tickets = [...userTickets];
-    tickets = tickets.filter((number) => number.number !== numberToDelete);
-    setUserTickets(tickets);
-  }
 
   async function getRaffleInfo() {
     if (!raffleId) return;
     const raffleData: RaffleResponse = await fetchRaffle(+raffleId);
     setRaffle(raffleData);
-  }
-
-  function changeGrid(advance: boolean) {
-    if (advance) next();
-    else previous();
   }
 
   useEffect(() => {
@@ -51,69 +34,9 @@ function Raffle() {
             <h2 className="section-header mb-2">Grab your tickets</h2>
             <p className="h5">{raffle?.title}</p>
           </div>
-
-          <UserTickets
-            removeNumber={handleRemoveTicket}
-            numbers={userTickets}
-          />
         </div>
 
-        <div className="my-5 text-center">
-          <h2 className="section-header mb-3">Grid</h2>
-          <p className="m-0 p-0 mb-1">You can select * numbers</p>
-          <p className="m-0 p-0 mb-1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit,
-            voluptatem!
-          </p>
-          <hr className="my-4" />
-
-          <div>
-            <div className="d-flex justify-content-between mb-3">
-              <div className="me-3 text-start">
-                <p className="m-0 p-0">
-                  <strong>Page </strong>
-                  {page}
-                </p>
-                <p className="m-0 p-0 text-muted fst-italic">
-                  Showing <strong>{gridFetchLimit}</strong> tickets
-                </p>
-              </div>
-              <div>
-                <button
-                  onClick={() => changeGrid(false)}
-                  className="btn btn-primary btn-lg"
-                >
-                  {"<"}
-                </button>
-                <button
-                  onClick={() => changeGrid(true)}
-                  className="btn btn-primary btn-lg ms-1"
-                >
-                  {">"}
-                </button>
-              </div>
-            </div>
-            <div className="tickets-container">
-              {gridTickets.length === 0 && (
-                <div>
-                  <p className="m-0 p-0">
-                    <strong>There are no tickets</strong>
-                  </p>
-                  <p className="m-0 p-0 text-muted fst-italic">
-                    Try usign the pagination to return
-                  </p>
-                </div>
-              )}
-              {gridTickets.map((ticket) => {
-                return (
-                  <div key={ticket.number} className="ticket">
-                    {ticket.formatted}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
+        {raffle && <TicketsContainer raffleId={raffle?.id} />}
       </div>
     </div>
   );
